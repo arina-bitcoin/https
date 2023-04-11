1deb8c0d4af6956714ba45488488478fc7b6512b
sonya <sophia.grachova@gmail.com>
2023-04-11 22:36:14 +0300

    2

--- website/auth.py
+++ website/auth.py
@@ -3,3 +3,14 @@ from flask import Blueprint
 
 auth = Blueprint('auth', __name__)
 
+@auth.route('/sign-in')
+def sign_in():
+    return '<p>Sign in</p>'
+
+@auth.route('/sign-out')
+def sign_out():
+    return '<p>Sign out</p>'
+
+@auth.route('/sign-up')
+def sign_up():
+    return '<p>Sign up</p>'
